a9cc927f1d4e22704a561234f741ba73bdf2754e
maxcore25 <mkorobov2002@mail.ru>
2022-02-06 23:55:53 +0300

    Tiny Refactoring

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=origins,
+    allow_headers=origins,
 )
 
 
